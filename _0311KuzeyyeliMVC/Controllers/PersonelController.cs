89e37f49dbd2ab6c9c7fb76d39b8487fe28cf7d9
Ahmet Çankal <acankal@gmail.com>
2022-11-03 15:55:44 +0300

    Proje dosyası ekle.

---
+++ _0311KuzeyyeliMVC/Controllers/PersonelController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _0311KuzeyyeliMVC.Controllers
+{
+    public class PersonelController : Controller
+    {
+        // GET: Personel
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Ekle()
+        {
+            return View();
+        }
+
+        public ActionResult Liste()
+        {
+            NorthwindEntities db = new NorthwindEntities();
+            var sonuc = db.Personeller.ToList();
+
+            return View(sonuc);
+        }
+        public ActionResult Ara()
+        {
+            return View();
+        }
+        public ActionResult Sil()
+        {
+            return View();
+        }
+
+    }
+}
