0b8d98fea1618809d1af519fe2faa7b43679aad3
Ahmet Çankal <acankal@gmail.com>
2022-12-22 15:11:01 +0300

    arayuz added!

---
+++ _0311KuzeyyeliMVC/Controllers/KategoriController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _0311KuzeyyeliMVC.Controllers
+{
+    public class KategoriController : Controller
+    {
+        NorthwindEntities db = new NorthwindEntities();
+        // GET: Kategori
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult KatListe()
+        {
+            var katlist = db.Kategoriler.ToList();
+            return PartialView("_Header",katlist);
+        }
+
+
+    }
+}
---
+++ _0311KuzeyyeliMVC/Controllers/UrunlerController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _0311KuzeyyeliMVC.Controllers
+{
+    public class UrunlerController : Controller
+    {
+        NorthwindEntities db = new NorthwindEntities();
+        // GET: Urunler
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Liste(int id)
+        {
+            var sonuc = db.Urunler.Where(x=>x.KategoriID==id).ToList();
+            return View(sonuc);
+        }
+
+
+    }
+}
