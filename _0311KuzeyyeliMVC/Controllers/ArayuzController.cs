340e01d36fedf591405a84b5c0994d2690a81a92
Ahmet Çankal <acankal@gmail.com>
2022-12-22 14:27:45 +0300

    arayuz eklendi

---
+++ _0311KuzeyyeliMVC/Controllers/ArayuzController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _0311KuzeyyeliMVC.Controllers
+{
+    public class ArayuzController : Controller
+    {
+        // GET: Arayuz
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
